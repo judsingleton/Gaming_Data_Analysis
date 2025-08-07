bb0b6eb9d3ed57d70e419c902e0f4ca92f2f51f3
Jud <oberynmartell@gmail.com>
2025-08-07 09:45:09 -0400

    Added head, tail, and info checks on dataframes at logical spots in Jupyter notebook.  Ensured .py code was aligned with notebook other than removing the checks.

--- Gaming_Data_Analysis.py
+++ Gaming_Data_Analysis.py
@@ -4,54 +4,110 @@ import os
 import pandas as pd
 import sqlite3
 
-#Read each csv and create a corresponding pandas dataframe
-print(os.getcwd())
-
 sales_data = pd.read_csv("Video_Game_Sales_1978-2024.csv")
 top_50_data = pd.read_csv("2024_Top_50_AAA_AA_Indie_Games.csv")
 gaming_study_data = pd.read_csv("GamingStudy_data.csv", encoding='windows-1254')
 online_behavior_data = pd.read_csv("online_gaming_behavior_dataset.csv")
+genres = pd.read_csv('genres.csv')
 
-#Align column names and rename columns for clarity
+#Add normalization of genre data to top_50_data
+top_50_data = pd.merge(top_50_data, genres[['Name', 'Genre']], on='Name', how='left')
+
+#Align column names and rename columns for clarity.
 
 top_50_data.rename(columns={"ReleaseDate": "Year"}, inplace=True)
 gaming_study_data.rename(columns={"Game": "Name", "Hours": "HoursPerWeek", "Residence_ISO3": "Location", "GAD_T": "GAD_Total", "SWL_T": "SWL_Total", "SPIN_T": "SPIN_Total"}, inplace=True)
-online_behavior_data.rename(columns={"GameGenre": "Genre", "PlayTimeHours": "HoursPerWeek"}, inplace=True)
+online_behavior_data.rename(columns={"GameGenre": "Genre", "PlayTimeHours": "Hours per Week", "GameDifficulty": "Game Difficulty", "EngagementLevel": "Engagement Level", "InGamePurchases": "In Game Purchases", "SessionsPerWeek": "Sessions per Week", "AvgSessionDurationMinutes": "Average Session Duration"}, inplace=True)
 
-#Drop unnecessary columns from dataframe
+#Drop unnecessary columns from dataframe.
 
-sales_data = sales_data.drop(['Publisher', 'Developer', 'Critic_Score', 'All_Platforms', 'User_Score', 'All_Games', 'NA_Sales', 'PAL_Sales', 'JP_Sales', 'Other_Sales'], axis=1)
-top_50_data = top_50_data.drop(['Publishers', 'Developers', 'Steam Id'], axis=1)
+sales_data = sales_data.drop(['Rank', 'Publisher', 'Developer', 'Critic_Score', 'All_Platforms', 'User_Score', 'All_Games', 'NA_Sales', 'PAL_Sales', 'JP_Sales', 'Other_Sales'], axis=1)
+top_50_data = top_50_data.drop(['Publishers', 'Developers', 'Steam Id', 'Price', 'Review Count', 'Review Score', 'Steam Followers'], axis=1)
 top_50_data = top_50_data.drop(top_50_data.columns[0], axis=1)
-gaming_study_data = gaming_study_data.drop(['S. No.', 'Timestamp', 'GADE', 'earnings', 'whyplay', 'League', 'highestleague', 'streams', 'Birthplace', 'Residence', 'Reference', 'accept', 'Birthplace_ISO3'], axis=1)
-online_behavior_data = online_behavior_data.drop('PlayerID', axis=1)
+gaming_study_data = gaming_study_data.drop(['S. No.', 'Timestamp', 'GADE', 'earnings', 'whyplay', 'Degree', 'Playstyle', 'Work', 'League', 'highestleague', 'streams', 'Birthplace', 'Residence', 'Reference', 'accept', 'Birthplace_ISO3'], axis=1)
+online_behavior_data = online_behavior_data.drop(['PlayerID', 'PlayerLevel', 'AchievementsUnlocked'], axis=1)
+
+#Remove rows with NA for NAME
+top_50_data = top_50_data.dropna(subset='Name')
+sales_data = sales_data.dropna(subset='Name')
+gaming_study_data = gaming_study_data.dropna(subset='Name')
 
 #Convert any necessary columns to correct data type
-print(sales_data.info())
-sales_data['Name'] = sales_data['Name', 'Platform', 'Genre'].astype(str)
+sales_data['Name'] = sales_data['Name'].astype("string")
+sales_data['Platform'] = sales_data['Platform'].astype("string")
+sales_data['Genre'] = sales_data['Genre'].astype("string")
 
+top_50_data['Name'] = top_50_data['Name'].astype("string")
+top_50_data['Genre'] = top_50_data['Genre'].astype("string")
+top_50_data['PublisherClass'] = top_50_data['PublisherClass'].astype("string")
+top_50_data['Copies Sold'] = top_50_data['Copies Sold'].str.replace(',', '')
+top_50_data['Copies Sold'] = top_50_data['Copies Sold'].astype('float64')
+top_50_data['Copies Sold'] = top_50_data['Copies Sold'].astype(int)
+top_50_data['Gross Revenue'] = top_50_data['Gross Revenue'].str.replace(',', '')
+top_50_data['Gross Revenue'] = top_50_data['Gross Revenue'].str.replace('$', '')
+top_50_data['Gross Revenue'] = top_50_data['Gross Revenue'].astype('float64')
 
-#Remove all game series from sales data as well as cross-platform entries as these have no sales figures and/or are also broken out by entry and platform
+gaming_study_data['Name'] = gaming_study_data['Name'].astype("string")
+gaming_study_data['Gender'] = gaming_study_data['Gender'].astype("string")
+gaming_study_data['Platform'] = gaming_study_data['Platform'].astype("string")
+gaming_study_data['Location'] = gaming_study_data['Location'].astype("string")
+
+online_behavior_data['Gender'] = online_behavior_data['Gender'].astype("string")
+online_behavior_data['Location'] = online_behavior_data['Location'].astype("string")
+online_behavior_data['Genre'] = online_behavior_data['Genre'].astype("string")
+online_behavior_data['Game Difficulty'] = online_behavior_data['Game Difficulty'].astype("string")
+online_behavior_data['Engagement Level'] = online_behavior_data['Engagement Level'].astype("string")
+
+#Remove rows with NA for SPIN
+gaming_study_data = gaming_study_data.dropna(subset='SPIN_Total')
+
+#Clean up the Platform data - only PC and console are differentiated so no need for the special characters. Similarly Smartphone / Tablet can be consolidated as Mobile.
+gaming_study_data['Platform'] = gaming_study_data['Platform'].replace('Console (PS, Xbox, ...)', 'Console')
+gaming_study_data['Platform'] = gaming_study_data['Platform'].replace('Smartphone / Tablet', 'Mobile')
+
+#The names of the games on the gaming study data should match those on the sales data sheet as well.
+
+gaming_study_data['Name'] = gaming_study_data['Name'].replace('Skyrim', 'The Elder Scrolls V: Skyrim')
+gaming_study_data['Name'] = gaming_study_data['Name'].replace('Counter Strike', 'Counter-Strike')
+gaming_study_data['Name'] = gaming_study_data['Name'].replace('Diablo 3', 'Diablo III')
+
+#Clean up the Platform data - only PC and console are differentiated so no need for the special characters. Similarly Smartphone / Tablet can be consolidated as Mobile.
+gaming_study_data['Platform'] = gaming_study_data['Platform'].replace('Console (PS, Xbox, ...)', 'Console')
+gaming_study_data['Platform'] = gaming_study_data['Platform'].replace('Smartphone / Tablet', 'Mobile')
+
+#The names of the games on the gaming study data should match those on the sales data sheet as well.
+
+gaming_study_data['Name'] = gaming_study_data['Name'].replace('Skyrim', 'The Elder Scrolls V: Skyrim')
+gaming_study_data['Name'] = gaming_study_data['Name'].replace('Counter Strike', 'Counter-Strike')
+gaming_study_data['Name'] = gaming_study_data['Name'].replace('Diablo 3', 'Diablo III')
+
+#Remove all game series from sales data as well as cross-platform entries as these have no sales
 
 sales_data = sales_data.drop(sales_data[(sales_data['Platform'] == ('Series'))].index)
 sales_data = sales_data.dropna(subset="Global_Sales")
 
+#Update PokÃ©mon to Pokemon in the sales data. As there are various versions of 'Starcraft II' on the sales data sheet this is updated to 'Starcraft' to match the gaming study data.
+
+sales_data['Name'] = sales_data['Name'].str.replace('PokÃ©mon', 'Pokemon')
+sales_data.loc[sales_data['Name'].str.contains('StarCraft II'), 'Name'] = 'Starcraft 2'
+
+online_behavior_data['Genre'] = online_behavior_data['Genre'].replace('RPG', 'Role-Playing')
+
 #Utility function to get average value of one specified column based on given value in another column (defaults to Name).
 
-def get_average(temp_df, tlist, cname, kname='Name'):
+def get_average(temp_df, tlist: list[str], cname: str, kname: str='Name'):
+    """Calculates average value of specified column based on given value in another column (defaults to Name).
+    Inputs:  Pandas dataframe, list of unique values for key column, name of column to calculate, optional key column specification.
+    Outputs:  No return value; appends "cname_avg" column to temp_df."""
     for i in range(len(tlist)):
         mask = temp_df[kname] == tlist[i]
         average_val = temp_df[temp_df[kname] == tlist[i]][cname].mean()
         temp_df.loc[temp_df[kname] == tlist[i], [cname+'_avg']]= average_val
 
-#For games with multiple entries that have sales data, add the sum of Global_Sales to the title (not series) from all platforms for the title
-
-sales_data['Multiplatform'] = sales_data.duplicated(subset='Name', keep=False)
-
-
 def duplist(temp_df):
-    """Provides a list of values in the Name field  of the sales_data that appear more than once.  Only exact matches are returned."""
-    multi_series = sales_data['Name'].value_counts()
+    """Provides a list of values in the Name field  of the sales_data that appear more than once.  Only exact matches are returned.
+    For example, 'Tetris', 'Tetris DS', and 'Tetris Plus' are all considered unique values."""
+    multi_series = temp_df['Name'].value_counts()
     multi_list = multi_series[multi_series > 1].index.to_list()
     multi_list = list(set(multi_list))
     return multi_list
@@ -61,8 +117,6 @@ def duplist(temp_df):
 
 mask = sales_data['Name'].isin(duplist(sales_data))
 placeholder = sales_data[mask] #dataframe with only titles with more than one entry
-#might need to drop some or all NaN or zero entries for Global Sales
-
 placeholder = placeholder[placeholder['Global_Sales'] != 0.0]
 plist = placeholder['Name'].to_list()
 
@@ -73,19 +127,19 @@ for i in range(len(plist)):
     title_list.append(temp_df)
 
 def title_gs_calc(title_df):
-    """Takes a dataframe with multiple rows for a title and returns a datframe with a single row with the Global_Sales calculated."""
+    """Takes a dataframe with multiple rows for a title and returns a dataframe with a single row with the Global_Sales calculated."""
     return_df = title_df.head(1).reset_index()
     return_df['Platform'] = 'All'
     return_df['Global_Sales'] = title_df['Global_Sales'].sum()    
     return return_df
 
 def title_add(tlist):
-    """Takes a list of dataframe consisting of the same title and returns a dataframe that with All for the Platform value and the sum of the columns for Global_Sales."""
+    """Takes a list of dataframes consisting of the same title and returns a dataframe with All for the Platform value and the sum of the columns for Global_Sales."""
     added_titles = pd.DataFrame()
     for i in range(len(tlist)):
         temp_df = title_gs_calc(tlist[i])
         added_titles = pd.concat([added_titles, temp_df], ignore_index=True)
-        added_titles = added_titles.drop_duplicates()
+        added_titles = added_titles.drop_duplicates()     
     return added_titles
 
 sales_data = pd.concat([sales_data, title_add(title_list)], ignore_index=True)
@@ -98,6 +152,7 @@ rows_to_save = sales_data[sales_data['Platform'] == 'All']
 sales_data = sales_data.drop(rows_to_remove)
 sales_data = pd.concat([sales_data, rows_to_save], ignore_index=True)
 sales_data = sales_data.drop_duplicates()
+sales_data = sales_data.drop(columns=sales_data.columns[5], axis=1)
 
 #Create list from Name column of top_50_data to use for column name for new dataframe created from Tags column
 top_50_names = top_50_data['Name'].tolist()
@@ -124,11 +179,10 @@ for name in top_50_names:
     else:
         top_50_data.loc[name, 'Multiplayer'] = False
 
+#Now that we have the Singleplayer and Multiplayer tags extracted we can drop the Tags column
+top_50_data = top_50_data.drop(columns='Tags')
 
-#Remove rows with NA for SPIN
-gaming_study_data = gaming_study_data.dropna(subset='SPIN_Total')
-
-print(gaming_study_data.head())
+#Get column averages for specified and append to new columns at end
 
 gmask = gaming_study_data['Name'].unique().tolist()
 
@@ -137,16 +191,11 @@ get_average(gaming_study_data, gmask, 'SWL_Total')
 get_average(gaming_study_data, gmask, 'SPIN_Total')
 get_average(gaming_study_data, gmask, 'HoursPerWeek')
 
-print(gaming_study_data.head()) #REMOVE ME
-print(gaming_study_data.info()) #REMVOE ME
+#Create new dataframe with one entry per game.  The individual item averages for each screening test are not included in the dataframe
+# as it is intended to be a high-level overview.
 
 gaming_study_avg = gaming_study_data.filter(['Name', 'HoursPerWeek_avg', 'GAD_Total_avg', 'SWL_Total_avg', 'SPIN_Total_avg'], axis=1)
 gaming_study_avg = gaming_study_avg.drop_duplicates()
-gaming_study_avg.to_csv("gaming_study_avg.csv")
-
-print(gaming_study_avg.head()) #REMOVE ME
-
-
 
 #Open the SQL connection and create a cursor
 
@@ -162,12 +211,49 @@ def sql(query):
 sales_data.to_sql('Sales', conn, if_exists='replace', index=False)
 gaming_study_avg.to_sql('Gaming_Study_Avg', conn, if_exists='replace', index=False)
 
-sales_and_study = sql("""
-    SELECT *
-    FROM Gaming_Study_Avg
-    LEFT JOIN Sales ON Sales.Name = Gaming_Study_Avg.Name;
+conn.execute("""
+    CREATE TABLE SalesT
+    (
+    Name TEXT PRIMARY KEY,
+    Platform TEXT,
+    Global_Sales REAL,
+    Genre TEXT,
+    Year INTEGER);
+    """)
+    
+conn.execute("""
+    INSERT OR REPLACE INTO SalesT (Name, Platform, Global_Sales, Genre, Year)
+    SELECT Name, Platform, Global_Sales, Genre, Year FROM Sales;
     """)
 
-sales_and_study.to_csv('sales_and_study.csv') 
+conn.execute("""
+    CREATE TABLE GST
+    (
+    Name TEXT PRIMARY KEY,
+    HoursPerWeek_avg REAL,
+    GAD_Total_avg REAL,
+    SWL_Total_avg REAL,
+    SPIN_Total_avg REAL);
+    """)
+
+conn.execute("""
+    INSERT OR REPLACE INTO GST (Name, HoursPerWeek_avg, GAD_Total_avg, SWL_Total_avg, SPIN_Total_avg)
+    SELECT Name, HoursPerWeek_avg, GAD_Total_avg, SWL_Total_avg, SPIN_Total_avg FROM Gaming_Study_Avg;
+    """)
+
+sales_and_study = sql("""
+    SELECT *
+    FROM GST
+    LEFT JOIN SalesT ON SalesT.Name = GST.Name;
+    """)
+sales_and_study = sales_and_study.loc[:,~sales_and_study.columns.duplicated()]
 
 conn.close()
+
+#Export our updated and new dataframes to CSV to be loaded into Tableau!
+
+sales_data.to_csv('Final_Data/Sales.csv', index=False)
+top_50_data.to_csv('Final_Data/Top_50.csv', index=False)
+gaming_study_data.to_csv('Final_Data/Gaming_Study.csv', index=False)
+online_behavior_data.to_csv('Final_Data/Behavior.csv', index=False)
+sales_and_study.to_csv('Final_Data/Sales_and_Study.csv', index=False)
